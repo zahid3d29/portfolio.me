8f5c120ab79c06b9846bcaaba803124e07e837b7
zahid3d29 <zahid3d29@gmail.com>
2026-09-08 23:53:22 +0600

    Purano shob delete kore notun file upload kora holo

--- js/zahid3d9.js
+++
@@ -1,8 +0,0 @@
-// code for mobile navigation 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
